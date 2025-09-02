37268500e82b392c2d32107e82917c0eefa54bf4
李勇 <2236472089@qq.com>
2025-09-02 23:07:15 +0800

    feat: 使用 mock 数据

--- packages-app/demo-init/src/app.tsx
+++ packages-app/demo-init/src/app.tsx
@@ -149,7 +149,7 @@ export const layout: RunTimeLayoutConfig = ({
  * 它基于 axios 和 ahooks 的 useRequest 提供了一套统一的网络请求和错误处理方案。
  * @doc https://umijs.org/docs/max/request#配置
  */
-export const request: RequestConfig = {
-  baseURL: 'https://proapi.azurewebsites.net',
-  ...errorConfig,
-};
+// export const request: RequestConfig = {
+//   baseURL: 'https://proapi.azurewebsites.net',
+//   ...errorConfig,
+// };
