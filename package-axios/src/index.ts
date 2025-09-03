ad93ec49bd130f4a273c8eb86415ff7ca11eaa8e
李勇 <2236472089@qq.com>
2025-09-04 00:30:47 +0800

    chore: 添加 package-axios 包及其配置文件，更新 pnpm-workspace.yaml 中的 TypeScript 版本

---
+++ package-axios/src/index.ts
@@ -0,0 +1,77 @@
+import RequestClient, {
+  authenticateResponseInterceptor,
+  defaultResponseInterceptor,
+  errorMessageResponseInterceptor,
+  formatToken,
+  RequestClientOptions
+} from "@mt-kit/request-axios";
+
+/**
+ * 接口地址
+ *
+ * 需要启动 mock 服务
+ */
+
+function createRequestClient(
+    baseUrl: string = "",
+    options?: RequestClientOptions
+): RequestClient {
+  const client = new RequestClient({
+    ...options,
+    baseURL: baseUrl
+  });
+
+  /**
+   * 重新认证逻辑
+   *
+   * 退出登陆
+   */
+  async function doReAuthenticate(): Promise<void> {
+    console.warn("Access token or refresh token is invalid or expired. ");
+  }
+
+  /**
+   * 刷新token逻辑
+   */
+  async function doRefreshToken(): Promise<string> {
+    console.warn("刷新 token 逻辑");
+
+    return "";
+  }
+
+  // 请求头处理
+  client.addRequestInterceptor({
+    fulfilled: async config => {
+
+      config.headers.Authorization = formatToken("");
+
+      return config;
+    }
+  });
+
+  client.addResponseInterceptor(defaultResponseInterceptor());
+
+  // token过期的处理
+  client.addResponseInterceptor(authenticateResponseInterceptor({
+    client,
+    doReAuthenticate,
+    doRefreshToken,
+    enableRefreshToken: true,
+    formatToken
+  }));
+
+  // 通用的错误处理,如果没有进入上面的错误处理逻辑，就会进入这里
+  client.addResponseInterceptor(errorMessageResponseInterceptor({
+    errorFn: error => {
+      console.error(error);
+    }
+  }));
+
+  return client;
+}
+
+const requestClient = createRequestClient();
+
+export default requestClient;
+
+export const baseRequestClient = new RequestClient();
