e2457b6e23743cd6e6b11c18c9d6bd8bc177c494
李勇 <2236472089@qq.com>
2025-09-04 23:15:56 +0800

    chore: 更新 pnpm-workspace.yaml，添加 antd 依赖并调整 package.json 中的依赖项，同时注释掉开发环境中的 findDOMNode 警告处理代码

--- packages-app/demo/src/pages/login/index.tsx
+++ packages-app/demo/src/pages/login/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import {
+  Button
+} from "antd";
+
 export default function Login(): React.ReactElement {
-  return <div>Login</div>;
+  return <div>
+    <Button type="primary">登录</Button>
+  </div>;
 }
