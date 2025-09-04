63dccc19f46a4c4b87c07518c84d708b6311013e
李勇 <2236472089@qq.com>
2025-09-04 23:18:30 +0800

    feat: 添加重定向功能到登录按钮

--- packages-app/demo/src/pages/login/index.tsx
+++ packages-app/demo/src/pages/login/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {
+  useCallback
+} from "react";
 
 import {
   Button
 } from "antd";
 
 export default function Login(): React.ReactElement {
+  const handleClick = useCallback(() => {
+    const urlParams = new URL(window.location.href).searchParams;
+
+    window.location.href = urlParams.get("redirect") || "/";
+  }, []);
+
   return <div>
-    <Button type="primary">登录</Button>
+    <Button onClick={handleClick}
+      type="primary">
+      登录
+    </Button>
   </div>;
 }
