93ee5ab9cd9f573c0d7e0967cbeae9258108c09a
李勇 <yong.li@yidatec.com>
2025-09-05 11:02:03 +0800

    feat: 获取用户信息

--- packages-app/demo/src/pages/index/index.tsx
+++ packages-app/demo/src/pages/index/index.tsx
@@ -1,13 +1,24 @@
-export default function HomePage() {
+import {
+  useModel
+} from "@umijs/max";
+
+export default function HomePage(): React.ReactElement {
+
+  const {
+    initialState
+  } = useModel("@@initialState");
+
   return (
     <div>
       <h2>Yay! Welcome to umi!</h2>
 
       <p>
         To get started, edit
-        {" "}
+        {initialState?.username}
         <code>pages/index.tsx</code>
-        {" "}
+        {initialState?.email}
+        {initialState?.avatar}
+        {initialState?.phone}
         and save to reload.
       </p>
     </div>
